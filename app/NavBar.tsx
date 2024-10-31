9208869c4a23b0ca1e41e7177688e1a8255f8faa
Osama Yeslam <os7softgithub@gmail.com>
2024-10-31 18:08:27 +0300

    feat: build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { IoBug } from "react-icons/io5";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 items-center h-14 px-5 mb-6 border-b">
+      <Link href="/">
+        <IoBug size={25} />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-700 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
