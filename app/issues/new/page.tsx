ec83e4675993fb00ef316c3922a4775520198421
Osama Yeslam <os7softgithub@gmail.com>
2024-11-01 19:08:36 +0300

    feat: handling with error api

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,42 +1,54 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
 import { useForm, Controller, Form } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import SimpleMDE from "react-simplemde-editor";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { z } from "zod";
+import { createIssueSchema } from "@/app/validationSchema";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+  const [error, setError] = useState<any>("");
   const router = useRouter();
   const form = useForm<IssueForm>();
 
   return (
-    <form
-      onSubmit={form.handleSubmit(async (data) => {
-        await axios.post("http://localhost:3000/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <div className="max-w-xl space-y-3">
-        <TextField.Root {...form.register("title")} placeholder="Title">
-          <TextField.Slot></TextField.Slot>
-        </TextField.Root>
-        <Controller
-          name="description"
-          control={form.control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description..." {...field} />
-          )}
-        />
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-3">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        onSubmit={form.handleSubmit(async (data) => {
+          try {
+            await axios.post("http://localhost:3000/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexepted error occurred.");
+          }
+        })}
+      >
+        <div className="max-w-xl space-y-3">
+          <TextField.Root {...form.register("title")} placeholder="Title">
+            <TextField.Slot></TextField.Slot>
+          </TextField.Root>
+          <Controller
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description..." {...field} />
+            )}
+          />
 
-        <Button>Submit</Button>
-      </div>
-    </form>
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
