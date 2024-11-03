76b28948d7395c2b386ea76ddf1a7efbdc82e980
Osama Yeslam <os7softgithub@gmail.com>
2024-11-03 21:19:38 +0300

    feat: build api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,32 @@
+import { issueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const issue = await prisma.issue.findUnique({ where: { id: params.id } });
+
+  if (!issue)
+    return NextResponse.json(
+      { error: "This issue not founded" },
+      { status: 400 }
+    );
+
+  const issueUpdated = await prisma.issue.update({
+    where: { id: params.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(issueUpdated, { status: 200 });
+}
