93637b74e33224a9e88d35c7d371b530578f1c1d
Osama Yeslam <os7softgithub@gmail.com>
2024-11-05 20:33:45 +0300

    refactor: refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,14 +14,8 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBug } from "react-icons/io5";
 import { useSession } from "next-auth/react";
-const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data } = useSession();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
 
+const NavBar = () => {
   return (
     <nav className="py-3 px-5 mb-6 border-b">
       <Container>
@@ -30,55 +24,78 @@ const NavBar = () => {
             <Link href="/">
               <IoBug size={25} />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-700 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={data.user!.image!}
-                    fallback="?"
-                    radius="full"
-                    size="2"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content size="1">
-                  <DropdownMenu.Label>
-                    <Text size="2">{data.user!.email!}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Separator />
-                  <DropdownMenu.Item>
-                    <Text>
-                      <Link href={"/api/auth/signout"}>Logout</Link>
-                    </Text>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href={"/api/auth/signin"}>Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href={"/api/auth/signin"}>
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={data!.user!.image!}
+            fallback="?"
+            radius="full"
+            size="2"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content size="1">
+          <DropdownMenu.Label>
+            <Text size="2">{data!.user!.email!}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item>
+            <Text>
+              <Link href={"/api/auth/signout"}>Logout</Link>
+            </Text>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
